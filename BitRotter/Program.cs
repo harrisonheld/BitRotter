cc5c8c1d64f6d281b24f5d1759ea654e86a1e4c9
Harrison Held <harrydheld@gmail.com>
2022-01-31 11:14:27 -0600

    Add project files.

---
+++ BitRotter/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace BitRotter
+{
+    class Program
+    {
+        const string FILE = "C:/Users/johnd/Desktop/test.txt";
+        const string OUTPUT = "C:/Users/johnd/Desktop/output/";
+        static void Main(string[] args)
+        {
+            byte[] bytes = File.ReadAllBytes(FILE);
+
+            for(int b = 0; b < bytes.Length; b++)
+            {
+                byte currByte = bytes[b];
+
+                for(int n = 0; n < 8; n++)
+                {
+                    // flip the nth bit
+                    bytes[b] = FlipBit(currByte, n);
+
+                    // save the file
+                    string fileName = b.ToString() + "_" + n.ToString() + ".txt";
+                    File.WriteAllBytes(OUTPUT + fileName, bytes);
+
+                    // restore the original byte
+                    bytes[b] = currByte;
+                }
+            }
+        }
+
+        // flip the nth bit (0 indexed)
+        private static byte FlipBit(byte b, int n)
+        {
+            return (byte)(b ^ (1 << n));
+        }
+    }
+}
